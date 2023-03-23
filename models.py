cc0a51fff990eadd17718262787ed2c77ae4c475
GertyPieters <119414044+GertyPieters@users.noreply.github.com>
2023-03-23 16:59:18 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,42 @@
+import peewee
+
+db = peewee.SqliteDatabase("betsy.db")
+
+class User(peewee.Model):
+    name = peewee.CharField()
+    address = peewee.CharField()
+    iban = peewee.CharField()
+
+    class Meta:
+        database = db
+
+class Tag(peewee.Model):
+    name = peewee.CharField()
+
+    class Meta:
+        database = db
+
+class Product(peewee.Model):
+    name = peewee.CharField()
+    description = peewee.TextField ()
+    price_per_unit = peewee.DecimalField(decimal_places=2)
+    quantity = peewee.IntegerField()
+    owner = peewee.ForeignKeyField(User, backref='products')
+    tags = peewee.ForeignKeyField(Tag, backref='products')
+
+    class Meta:
+        database = db
+ 
+
+class Transaction(peewee.Model):
+    buyer = peewee.ForeignKeyField(User, backref='transactions')
+    product = peewee.ForeignKeyField(Product, backref='transactions')
+    quantity = peewee.IntegerField()
+
+
+    class Meta:
+        database = db   
+
+
+
+    
